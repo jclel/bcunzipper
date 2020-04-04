470f24377853cb084a1a72c7d41d2391a3218775
Jake Cleland <s3328070@student.rmit.edu.au>
2020-04-04 17:43:58 +1100

    v2.0

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from zipfile import ZipFile 
 import os
-from pathlib import Path
-import shutil
 import glob
+
 
 print("****************************************************")
 print("****************************************************")
@@ -26,107 +25,110 @@ print( "                                                   ")
 
 print("Drag the zip files onto this window and press enter")
 
-file_name = input()
+filenames = input()
 
 # Handling multipe zip files
-list_of_files = file_name.split('"')
+list_of_files = filenames.split('"')
 files_to_unzip = []
 # Remove blank items from list
 for zips in list_of_files:
     if zips is not '':
         files_to_unzip.append(zips)
+
+# Set the root dir to wherever this is run from.
+root_dir = os.getcwd()
+
+def unzip(file):
+    # Set where we're going to unzip the files to
+    # Which is whatever is returned after artist/album folders are created
+    zip_destination = createFolders(file)
+    # Make sure we're in the same directory as the program and the zip files.
+    os.chdir(root_dir)
+    # If we've actually created folders, we can unzip them.
+    # If createFolders() returned 0, we didn't make new folders, so we skip it altogether.
+    # And you can reflect on how better to keep track of what songs you already have ;)
+    if zip_destination is not 0:
+        print("Unzipping zip file from " + root_dir)
+        with ZipFile(file, 'r') as zip:
+            # Change to newly created folders
+            os.chdir(zip_destination)
+            print("And unzipping file to " + zip_destination)
+            # Unzip contents into there
+            print("Extracting...")
+            zip.extractall()
+
+    # Return to root folder to do it all again for the next zip file.
+    os.chdir(root_dir)
+
+
+def getArtistAndAlbumName(file):
+    # Get the artist and album name by splitting the filename by the '-'
+    array = file.split(" - ")
+    return array
     
 
-def unzip(file_name):
-    # Get the root directory to start every time this is run
-    # After traversing directories later, we can use this variable to return to where all our zips are
-    root_dir = os.getcwd()
-
-
-    # Get the artist and album name by splitting the filename by the '-'
-    array = file_name.split(" - ")
+def checkFolders(file):
+    # Make a list of all the folders in the root directory.
+    root_folders = os.listdir(os.getcwd())
+    # Retrieve artist and album name by splitting zip file name.
+    array = getArtistAndAlbumName(file)
     artist_name = array[0]
     album_name = array[1]
     # Remove .zip from folder name with a substring
     album_name = album_name[:-4]
 
+    dir_to_create = ""
+    
+    for root_folder in root_folders:
+        # If the artist already exists
+        if root_folder in artist_name:
+            # Check for contents of artist folder.
+            os.chdir(artist_name)
+            print(f"Checking folder in {os.getcwd()}")
 
-# WRAP IT IN A TRY BLOCK FUCK IT
-
-    # Make a list of folders in the current directory
-    list_of_existing_folders = os.listdir(root_dir) # pyunzip
-    for folder in list_of_existing_folders:
-        # If a folder for the artist already exists
-        if folder in artist_name:
-            for album in os.listdir(artist_name):
-                if album_name in album:
-                    print("Found album.")
-                else:
-                    dirname = artist_name + "/" + album_name + "/"
+            # If there aren't any folders in the artist folder
+            # Pass album name as the folder to create
+            if not os.listdir(os.getcwd()):
+                dir_to_create = album_name
+            else:
+                # If the album already exists, we won't make the folder again.
+                for albums in os.listdir(os.getcwd()):
+                    if albums in album_name:
+                        # Don't create any new folders
+                        dir_to_create = 0
+                        return dir_to_create
+                    # If other albums exist but not this one
+                    # Pass current album as the folder to create
+                    # And return to root directory.
+                    else:
+                        print("Making album name folder " + album_name )
+                        dir_to_create = album_name
+                        os.chdir(root_dir)
+        # If the artist doesn't exist, pass artists and album folders to be created.
         else:
-            dirname = artist_name + "/" + album_name + "/"
-        # if folder in artist_name:
-        #     # Change to the artist folder
-        #     os.chdir(artist_name) # pyunzip/artist_name/
-        #     new_dir = os.getcwd() # pyunzip/artist_name/
-        #     list_of_albums = os.listdir(new_dir) # pyunzip/artist_name/
-        #     for album in list_of_albums:
-        #         if album in album_name:
-        #             print(f"{album} already exists.")
-        #             os.chdir(root_dir)
-        #             break
-        #         else: # if album doesn't exist
-        #             # And only create an album name folder
-        #             dirname = album_name + "/"
-                   
-        #             os.chdir(root_dir)
-                    
-            
-        # else:
-        #     # Create artist and album folders
-        #     dirname = artist_name + "/" + album_name + "/"
-            
-   
+            dir_to_create = artist_name + "/" + album_name + "/"
 
-    os.makedirs(dirname)
-    print("CURRENT DIR: " + os.getcwd())
-    with ZipFile(file_name, 'r') as zip:
-        # Change to newly created folders
-        os.chdir(dirname)
-        # Unzip contents into there
-        print("Extracting...")
-        zip.extractall()
-        
-    print("CURRENT DIR: " + os.getcwd())
-    # Create a list of newly unzipped songs
-    # We use glob to pattern match the filenames so we don't accidentally rename cover images etc
-    songs = []
-    # Only get mp3s
-    for file in glob.glob("*.mp3"):
-        songs.append(file)
+    return dir_to_create
 
-    for song in songs:
-        # Remove all the stuff in the filename before the '-' (e.g. artist name)
-        song_title = song.split(" - ")
-        # Tell the system the actual song title is the part that comes AFTER the '-'
-        song_title = song_title[-1]
-        # Print changes being made to the console
-        print(f"Changing {song} -> {song_title}")
-        # Rename the songs
-        os.rename(song, song_title)
-    # Change back to our original directory, ready to unzip some more songs
+def createFolders(file):
+ 
+    dir_to_create = checkFolders(file)
     os.chdir(root_dir)
-
-
+    
+    if dir_to_create is 0:
+        print("Found! Skipping...")
+        return 0
+    else:
+        # Create whatever folders were returned from checkFolders()
+        os.makedirs(dir_to_create)
+        print(f"Made folder in {os.getcwd()}: {dir_to_create}")
+    # Change directory to the folder we just created.
+    os.chdir(dir_to_create)
+    # And pass that folder back to the calling method.
+    dir_to_unzip_to = os.getcwd()
+    return dir_to_unzip_to
 
 for file in files_to_unzip:
-    # Create another array with the constituent parts of the absolute path
-    filepath_parts = file.split("\\")
-    # Just get the last part (the actual filename w/o dir names)
-    actual_filename = filepath_parts[-1]
-    # Run the unzip function
-    unzip(actual_filename)
-
-
-print("\nFinished! Press enter to quit.")
-endtime = input()
+    # For every zip file passed to the program, unzip and organise.
+    # And pray it doesn't break.
+    unzip(file)
